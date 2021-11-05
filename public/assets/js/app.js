c4907dc0184ad548a7edabd39b6e8ccbc6b122aa
BryanBerger98 <bryan.berger98@gmail.com>
2021-11-05 17:18:11 +0100

    integrate google maps api

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,75 +1,90 @@
-let trackingStatus = false;
+let map;
+let lastRouteDistance = 0;
+function initMap() {
+    navigator.geolocation.getCurrentPosition(
+        success => {
+            const position = {lat: success.coords.latitude, lng: success.coords.longitude}
+            map = new google.maps.Map(document.getElementById("map"), {
+              zoom: 18,
+              center: position,
+              disableDefaultUI: true
+            });
+            const marker = new google.maps.Marker({
+              position: position,
+              map: map,
+            });
+        },
+        console.error,
+        {enableHighAccuracy: true}
+    );
+}
 
-// const waypoints = [];
-const waypoints = [
-    L.LatLng(48.853224, 2.403283),
-    L.LatLng(48.853264, 2.403372),
-    L.LatLng(48.853283, 2.403450),
-    L.LatLng(48.853306, 2.403525),
-    L.LatLng(48.853310, 2.403587),
-    L.LatLng(48.853321, 2.403635)
-];
-const myMap = L.map('mapid', {attributionControl: false});
-
-myMap.locate({setView: true, enableHighAccuracy:true})
-.on('locationfound', e => {
-    const radius = e.accuracy / 2;
-    L.marker(e.latlng).addTo(myMap);
-    L.circle(e.latlng, radius).addTo(myMap);
-}).on('locationerror', error => {
-    console.error(error);
-});
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 22,
-    maxNativeZoom: 22,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYnJ5YW5iZXJnZXIiLCJhIjoiY2t1dHVmcGV6M2VyZTJ3bzZlbGFqdXhtYiJ9.4X4rCNbG2QiEOD3pdV_6VA'
-}).addTo(myMap);
-
-// const routeControl = L.Routing.control({
-//     waypoints: [
-//         L.latLng(48.853224, 2.403283),
-//         L.latLng(48.853321, 2.403635)
-//     ]
-// });
-
-// routeControl.addTo(myMap);
-// console.log('Plan >>>', routeControl.getPlan())
-// console.log('Router >>>', routeControl.getRouter())
-
+const waypoints = [];
 
 function startTracking() {
-    trackingStatus = true;
-    alert('Tracking started');
     document.getElementById('startTrackingBtn').className = 'd-none';
-    document.getElementById('stopTrackingBtn').className = 'btn btn-danger m-auto d-block';
-    myMap.locate({setView: true, watch: true, enableHighAccuracy:true}).on('locationfound', e => {
-        const radius = e.accuracy / 2;
-        waypoints.push(e.latlng);
-        // console.log(waypoints);
-        // const distance = waypoints && waypoints.length > 1 ? myMap.distance(waypoints[0], waypoints[1]) : null;
-        // console.log(distance, 'meters');
-        L.marker(e.latlng).addTo(myMap);
-        // .bindPopup("You are within " + radius + " meters from this point").openPopup();
-        L.circle(e.latlng, radius).addTo(myMap);
-    }).on('locationerror', error => {
-        console.error(error);
-    });
+    document.getElementById('stopTrackingBtn').className = "d-block btn btn-danger m-auto";
+    navigator.geolocation.watchPosition(
+        success => {
+            waypoints.push({lat: success.coords.latitude, lng: success.coords.longitude});
+        },
+        console.error,
+        {enableHighAccuracy: true}
+    );
 }
 
 function stopTracking() {
     document.getElementById('stopTrackingBtn').className = 'd-none';
-    document.getElementById('startTrackingBtn').className = 'btn btn-primary m-auto d-block';
-    trackingStatus = false;
-    myMap.stopLocate();
-    L.Routing.plan(waypoints, {
-        addWaypoints: false,
-        draggableWaypoints: false
-    }).addTo(myMap);
+    document.getElementById('startTrackingBtn').className = "d-block btn btn-primary m-auto";
+    snapToRoad(waypoints);
+}
+
+function displayDistance(distance) {
+    if (distance < 1000) {
+        return distance.toFixed(2) + ' m';
+    }
+    return (distance * 1000).toFixed(2) + ' km';
+}
+
+function snapToRoad(waypoints) {
+    const wps = waypoints.map(el => el.lat + ',' + el.lng).join('|');
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://roads.googleapis.com/v1/snapToRoads?path=${wps}&key=AIzaSyChZXDPZZW3HJIBpCtC7cJ6XAk_EGvofwc`, true);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            const pathWaypoints = JSON.parse(request.response).snappedPoints.map(el => {
+                console.log(el.location.latitude);
+                return new google.maps.LatLng(
+                    el.location.latitude,
+                    el.location.longitude);
+            });
+            const lastRoute = new google.maps.Polyline({
+                path: pathWaypoints,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+            });
+            lastRoute.setMap(map);
+            lastRouteDistance = google.maps.geometry.spherical.computeLength(lastRoute.getPath().getArray());
+            const textDistance = document.getElementById('text-distance');
+            textDistance.className = 'd-block text-center';
+            textDistance.innerText = displayDistance(lastRouteDistance);
+        }
+    }
+    request.send();
+    
+}
+
+function saveRouteToFirebase({waypoints, distance, startDate, endDate}) {
+    const ref = firebase.database().ref('routes/');
+    const newRouteRef = ref.push();
+    newRouteRef.set({
+        waypoints,
+        distance,
+        startDate,
+        endDate
+    });
+    
 }
 
 document.getElementById('startTrackingBtn').addEventListener('click', startTracking);
---
+++ public/assets/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4Hs2eOau0G3VVwHBu4kT4jlbo6ZVFQ2U",
+    authDomain: "location-tracker-479e9.firebaseapp.com",
+    databaseURL: "https://location-tracker-479e9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "location-tracker-479e9",
+    storageBucket: "location-tracker-479e9.appspot.com",
+    messagingSenderId: "848560678507",
+    appId: "1:848560678507:web:d05bd99617c56c6229f32e"
+};
+firebase.initializeApp(config);
+  
